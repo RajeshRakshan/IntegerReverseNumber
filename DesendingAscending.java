f40e966c5d025411a651ae278692e20bdb739a57
RajeshRakshan <111171348+RajeshRakshan@users.noreply.github.com>
2022-08-25 21:23:50 +0530

    DesendingAscendingArray
    Array value dispalying added

--- DesendingAscending.java
+++ DesendingAscending.java
@@ -1,27 +1,42 @@
 import java.util.Scanner;
 
-public class DesendingAscending {
+public class DesendingAscending 
+{
 	public static void main(String[] args) {
 	System.out.println("Enter the array size");
 	Scanner sc=new Scanner(System.in);
 	int i = sc.nextInt();
 	int a[]=new int[i];
 for (int j=0;  j< a.length; j++) {
+
 	a[j] = sc.nextInt();
-                                                      }
+                                 }
+System.out.println("Entered array value are ");
+for(int x:a){
+
+      System.out.print(" "+x);
+      
+            }
+
+System.out.println();
 System.out.println("Desending order:");
-for (int k = 0; k < a.length; k++) {
-	for (int l =k+1; l < a.length; l++) {
-		if(a[k]<a[l]) {//23653
+
+for (int k = 0; k < a.length; k++) 
+{
+	for (int l =k+1; l < a.length; l++) 
+     {
+		if(a[k]<a[l]) 
+          {
 			int tem=a[k];
 			a[k]=a[l];
 			a[l]=tem;
-		                        }
+	     }
 		
-	                                                            }
+	                                                                                                                              }
+
 	System.out.println(a[k]);
 	
-                                                       }
+}
 System.out.println("Ascending order:");
 for (int k = 0; k < a.length; k++) {
 	for (int l =k+1; l < a.length; l++) {
@@ -29,12 +44,16 @@ for (int k = 0; k < a.length; k++) {
 			int tem=a[k];
 			a[k]=a[l];
 			a[l]=tem;
-		                        }
+		               }
 		
-	                                                        }
+	                                    }
 	System.out.println(a[k]);
 	
-                                                 }
+                                    }
+
+
+
+
 
 
 	
